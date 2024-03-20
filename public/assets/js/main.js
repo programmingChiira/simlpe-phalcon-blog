a41267ca8574651df73b58b2db9d6dd733879b76
DennisChiira <programmingchiira@gmail.com>
2024-03-20 09:01:31 +0300

    updates

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,6 +1,6 @@
 /**
-* Template Name: Byte Beat
-* Template URL: https://bootstrapmade.com/Byte Beat-bootstrap-blog-template/
+* Template Name: Byte Bits
+* Template URL: https://bootstrapmade.com/Byte Bits-bootstrap-blog-template/
 * Updated: Mar 17 2024 with Bootstrap v5.3.3
 * Author: BootstrapMade.com
 * License: https:///bootstrapmade.com/license/
--- public/assets/scss/Readme.txt
+++ public/assets/scss/Readme.txt
@@ -1,2 +1,2 @@
 The .scss (Sass) files are only available in the pro version.
-You can buy it from: https://bootstrapmade.com/Byte Beat-bootstrap-blog-template/
+You can buy it from: https://bootstrapmade.com/Byte Bits-bootstrap-blog-template/
